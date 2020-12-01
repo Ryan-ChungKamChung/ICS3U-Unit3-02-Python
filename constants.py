e01c426ebf7ebf3b62d6bbe1b7b4d90a3c39559a
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-12-01 01:00:21 +0000

    Added constants.py and magic_number.py

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# Constants for magic_number.py
+
+MAGIC_NUMBER = 5
---
+++ magic_number.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in November 2020
+# Finding the magic number game
+
+
+import constants
+
+
+def main():
+    # This function finds the sum of 2 numbers
+
+    # Input
+    print("Guess the magic number (0-9)!")
+    magic_number = int(input("Please enter your guess: "))
+
+    # Process
+    if magic_number == constants.MAGIC_NUMBER:
+        win = 0
+
+    elif magic_number != constants.MAGIC_NUMBER:
+        win = 1
+    # Output
+    if win == 0:
+        print("Nice! Your answer is right!")
+
+    elif win == 1:
+        print("Oops! Your answer is wrong!")
+
+
+if __name__ == "__main__":
+    main()
