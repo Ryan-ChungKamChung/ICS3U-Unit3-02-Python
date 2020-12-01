bd2bfee65a00d136eac4d3b50d9d142750ddeae7
Ubuntu <ubuntu@ip-172-31-48-242.ec2.internal>
2020-12-01 01:22:54 +0000

    fixed magic_number.py

--- magic_number.py
+++ magic_number.py
@@ -12,21 +12,17 @@ def main():
     # This function finds the sum of 2 numbers
 
     print("Guess the magic number (0-9)!")
-    
+
     # Input
     magic_number = int(input("Please enter your guess: "))
 
     # Process
     if magic_number == constants.MAGIC_NUMBER:
-        win = 0
-
-    elif magic_number != constants.MAGIC_NUMBER:
-        win = 1
-    # Output
-    if win == 0:
+        # Output
         print("Nice! Your answer is right!")
 
-    elif win == 1:
+    elif magic_number != constants.MAGIC_NUMBER:
+        # Output
         print("Oops! Your answer is wrong!")
 
 
